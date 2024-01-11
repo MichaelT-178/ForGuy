a31e86d945043ec03d919d14c38739cdff533953
Michael Totaro <mtotaro@ncsu.edu>
2024-01-11 12:25:34 -0500

    JSON Data now working in SearchBox

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,32 +1,52 @@
 <template>
     <div v-if="showBox" class="modal">
       <div class="modal-content">
-        <button @click="closeModal">Close</button>
-        <slot></slot> <!-- Allows content to be inserted -->
+        <div class="content-container">
+          <slot>
+            <span style="color: black">HELLO</span>
+          </slot> 
+          <router-link 
+              v-for="item in searchMenu" 
+              :key="item.title" 
+              :to="item.link" 
+              @click="closeModal">
+              {{ item.title }}
+           </router-link>
+          <button @click="closeModal">Close</button>
+          <button @click="printJSON">Data</button>
+        </div>
       </div>
     </div>
   </template>
   
-  <script setup>
-  import { ref, defineProps, defineEmits, watchEffect } from 'vue';
+<script setup>
+import { ref, defineProps, defineEmits, watchEffect } from 'vue';
+import SearchData from '../data/SearchData.json';
+
+const props = defineProps({
+  isOpen: Boolean
+});
   
-  const props = defineProps({
-    isOpen: Boolean
-  });
+const emits = defineEmits(['update:isOpen']);
+const showBox = ref(props.isOpen);
   
-  const emits = defineEmits(['update:isOpen']);
+const closeModal = () => {
+  showBox.value = false;
+  emits('update:isOpen', false);
+};
   
-  const showBox = ref(props.isOpen);
-  
-  const closeModal = () => {
-    showBox.value = false;
-    emits('update:isOpen', false);
-  };
-  
-  watchEffect(() => {
-    showBox.value = props.isOpen;
-  });
-  </script>
+watchEffect(() => {
+  showBox.value = props.isOpen;
+});
+
+const jsonData = ref(SearchData);
+const searchMenu = jsonData.value;
+
+const printJSON = () => {
+  console.log(JSON.parse(JSON.stringify(searchMenu)))
+}
+
+</script>
   
   <style scoped>
   .modal {
@@ -47,6 +67,13 @@
     padding: 20px;
     border-radius: 5px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+  
+  .content-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    gap: 10px; 
   }
   </style>
   
