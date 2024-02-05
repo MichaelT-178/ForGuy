1ccc83458402ea5ba073cbe26b407ad141818dab
Michael Totaro <mtotaro@ncsu.edu>
2024-02-05 16:40:59 -0500

    Added icons to the Menu bar

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -8,50 +8,51 @@ export const menuItems = [
     {
         text: "Jobs",
         subMenu: [
-            { text: "Where To Apply", link: "/jobs/WhereToApply" },
-            { text: "Resume Template", link: "/jobs/ResumeTemplate" },
-            { text: "Offers I've Recieved", link: "/jobs/OffersRecieved" },
-            { text: "Interview Tips", link: "/jobs/InterviewTips" },
-            { text: "Helpful Websites", link: "/jobs/HelpfulWebsites" },
-            { text: "Job Tips", link: "/jobs/JobTips.vue" },
+            { text: "Where To Apply",        icon: "work",         link: "/jobs/WhereToApply" },
+            { text: "Resume Template",       icon: "description",  link: "/jobs/ResumeTemplate" },
+            { text: "Offers I've Recieved",  icon: "handshake",    link: "/jobs/OffersRecieved" },
+            { text: "Interview Tips",        icon: "group",        link: "/jobs/InterviewTips" },
+            { text: "Helpful Websites",      icon: "wysiwyg",      link: "/jobs/HelpfulWebsites" },
+            { text: "Job Tips",              icon: "lightbulb",    link: "/jobs/JobTips.vue" }
         ],
     },
     {
         text: "LinkedIn",
         subMenu: [
-            { text: "What Is LinkedIn?", link: "/linkedin/WhatIsLinkedin" },
-            { text: "Setup a Profile", link: "/linkedin/SetupProfile" },
-            { text: "Setup Job Alerts", link: "/linkedin/SetupJobAlerts" },
-            { text: "LinkedIn Tips", link: "/linkedin/LinkedinTips" },
+            { text: "What Is LinkedIn?",  icon: "person_add",      link: "/linkedin/WhatIsLinkedin" },
+            { text: "Setup a Profile",    icon: "account_circle",  link: "/linkedin/SetupProfile" },
+            { text: "Setup Job Alerts",   icon: "notifications",   link: "/linkedin/SetupJobAlerts" },
+            { text: "LinkedIn Tips",      icon: "psychology",      link: "/linkedin/LinkedinTips" }
         ],
     },
     {
         text: "Classes",
         subMenu: [
-            { text: "Class Recommendations", link: "/classes/ClassRecommendations" },
-            { text: "CSC Descriptions", link: "/classes/CSCDescriptions" },
-            { text: "Classes I've Taken", link: "/classes/ClassesIveTaken" },
-            { text: "Classes To Avoid", link: "/classes/ClassesToAvoid" },
+            { text: "Class Recommendations",  icon: "thumb_up",     link: "/classes/ClassRecommendations" },
+            { text: "CSC Descriptions",       icon: "description",  link: "/classes/CSCDescriptions" },
+            { text: "Classes I've Taken",     icon: "co_present",   link: "/classes/ClassesIveTaken" }, //interactive_space
+            { text: "Classes To Avoid",       icon: "cancel",       link: "/classes/ClassesToAvoid" }
         ],
     },
     {
         text: "Comp Sci",
         subMenu: [
-            { text: "Visual Studio Code", link: "/CompSci/VisualStudioCode" },
-            { text: "VSCode Settings", link: "/CompSci/VSCodeSettings" },
-            { text: "VSCode Shorcuts", link: "/CompSci/VSCodeShortcuts" },
-            { text: "Favorite Libraries", link: '/CompSci/FavoriteLibraries' },
-            { text: "Comp Sci Tips", link: "/CompSci/CompSciTips" },
+            { text: "Visual Studio Code",  icon: "laptop_mac",         link: "/CompSci/VisualStudioCode" },
+            { text: "VSCode Settings",     icon: "settings",           link: "/CompSci/VSCodeSettings" },
+            { text: "VSCode Shorcuts",     icon: "forward_media",      link: "/CompSci/VSCodeShortcuts" },
+            { text: "Extensions",          icon: "extension",          link: "/CompSci/VSCodeShortcuts" },
+            { text: "Favorite Libraries",  icon: "library_add_check",  link: "/CompSci/FavoriteLibraries" },
+            { text: "Comp Sci Tips",       icon: "device_hub",         link: "/CompSci/CompSciTips" } 
         ],
     },
     {
         text: "Other",
         subMenu: [
-            { text: "General Tips", link: "/other/GeneralTips" },
-            { text: "Apps To Download", link: "/other/AppsToDownload" },
-            { text: "CS Stuff I Like", link: "/other/CompSciStuff" },
-            { text: "Terminal Commands", link: "/other/TerminalCommands" },
-            { text: "Memes", link: "/other/Memes" },
+            { text: "General Tips",       icon: "self_improvement",  link: "/other/GeneralTips" },
+            { text: "Apps To Download",   icon: "phone_iphone",      link: "/other/AppsToDownload" },
+            { text: "CS Stuff I Like",    icon: "favorite",          link: "/other/CompSciStuff" },
+            { text: "Terminal Commands",  icon: "terminal",          link: "/other/TerminalCommands" },
+            { text: "Memes",              icon: "comedy_mask",       link: "/other/Memes" }
         ],
     },
     {
