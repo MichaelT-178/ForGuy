8baacadea7c5796f0a45e1ddfa89f036cec172c1
Michael Totaro <mtotaro@ncsu.edu>
2024-02-05 23:45:58 -0500

    Can now download Resume.docx

--- src/Views/jobs/ResumeTemplate.vue
+++ src/Views/jobs/ResumeTemplate.vue
@@ -2,31 +2,22 @@
     <main class="about-page">
         <h1>Resume Template</h1>
         <p>This is the Resume Template page</p>
-        <button @click="downloadDocx">Download DOCX</button>
-        <a download="COOL.docx" href="/Users/michaeltotaro/top-bar/src/data/jobs/ElijahResume.docx">YEET</a>
+        <button @click="downloadResumeDocx">Download DOCX</button>
     </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
-const downloadDocx = () => {
-    fetch("../../data/jobs/ElijahResume.docx")
-        .then(response => {
-            console.log(response)
-            console.log(response.url);
-        });
-        //.then(response => response.blob())
-        // .then(blob => {
-        //     const url = window.URL.createObjectURL(blob);
-        //     const a = document.createElement('a');
-        //     a.href = url;
-        //     a.download = 'filename.docx'; // Specify the file name
-        //     document.body.appendChild(a); // Required for this to work in FireFox
-        //     a.click();
-        //     a.remove(); // Cleanup
-        // })
-        // .catch(err => console.error('Download error:', err));
-}
+function downloadResumeDocx(){
+  const docxUrl = 'https://michaelt-178.github.io/TestWebsite/ResumeTemplate.docx';
+  const link = document.createElement('a');
+
+  link.href = docxUrl;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
 </script>
+
+
