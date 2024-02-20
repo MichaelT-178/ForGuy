a93293ae8c96682fc1815df29b4e80085c482393
Michael Totaro <mtotaro@ncsu.edu>
2024-02-20 17:22:28 -0500

    Added JSON Data and images

--- src/Views/linkedin/WhatIsLinkedin.vue
+++ src/Views/linkedin/WhatIsLinkedin.vue
@@ -1,6 +1,33 @@
 <template>
-    <main class="about-page">
-        <h1>What is Linkedin</h1>
-        <p>This is the Linkedin page</p>
-    </main>
+    <div class="image-container">
+        <img src="../../assets/linkedIn.png" alt="LinkedIn Pic" />
+        <div class="overlay"></div>
+    </div>
+    <h1>What is LinkedIn?</h1>
+    <p>This is the What is LinkedIn page</p>
 </template>
+
+<style scoped>
+
+.image-container {
+    position: relative;
+    width: 100%;
+    height: 400px;
+}
+
+img {
+    width: 100%; 
+    height: 100%;
+    display: block;
+}
+
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(8, 0, 255, 0.059);
+}
+
+</style>
