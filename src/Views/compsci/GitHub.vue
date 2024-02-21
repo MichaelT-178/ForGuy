4f00aa9e3ee41e1132cd1662e415446a5606e166
Michael Totaro <mtotaro@ncsu.edu>
2024-02-21 18:12:10 -0500

    Added GitHub, 12 Search bar results, and rearranged career path

---
+++ src/Views/compsci/GitHub.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="about-page">
+        <h1>GitHub</h1>
+        <p>This is the GitHub page</p>
+    </main>
+</template>
--- src/Views/compsci/VSCodeShortcuts.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <main class="about-page">
-        <h1>VS Code Shortcuts</h1>
-        <p>This is the VS Code Shortcuts page</p>
-    </main>
-</template>
