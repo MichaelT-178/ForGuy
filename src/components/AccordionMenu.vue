de0b7600e5c2bfc216927a482bb215d84be0dddd
Michael Totaro <mtotaro@ncsu.edu>
2024-02-25 17:43:40 -0500

    Added to HowToGetAJob view and updated AccordionMenu to handle arrays

--- src/components/AccordionMenu.vue
+++ src/components/AccordionMenu.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="accordion-menu">
     <div class="accordion-title" @click="toggleAccordion">
-        <h2>{{ item.Name }}</h2>
-        <span v-if="!isOpen"><span class="material-icons" id="open">add_circle</span></span>
-        <span v-else><span class="material-icons" id="close">cancel</span></span>
+      <h2>{{ item.Name }}</h2>
+      <span v-if="!isOpen"><span class="material-icons" id="open">add_circle</span></span>
+      <span v-else><span class="material-icons" id="close">cancel</span></span>
     </div>
     <div class="accordion-content" v-if="isOpen">
       <ul>
         <li v-for="(entry, index) in filteredEntries" :key="index">
-          <b> <u>{{ entry[0] }}</u>:</b> <span v-html="entry[1]"></span>
+          <b><u>{{ entry[0] }}</u>:</b>
+          <span v-if="typeof entry[1] === 'string'" v-html="entry[1]"></span>
+          <ul class="menu-attribute" v-else>
+            <li v-for="(item, idx) in entry[1]" :key="`item-${idx}`" v-html="formatEntry(item)" class="menu-items"></li>
+          </ul>
         </li>
       </ul>
     </div>
@@ -17,7 +21,7 @@
 
 <script setup>
 import { ref, computed, watchEffect, toRefs } from 'vue';
-import router from '../router/index.js'
+import router from '../router/index.js';
 
 const props = defineProps({
   item: Object,
@@ -33,13 +37,14 @@ watchEffect(() => {
 
 const toggleAccordion = () => {
   isOpen.value = !isOpen.value;
-}
+};
 
 const shouldDisplay = (key) => {
   const excludedKeys = ['Name'];
   return !excludedKeys.includes(key);
-}
+};
 
+//Ex: https://www.target.com/
 const highlightLinkText = (text) => {
   const urlPattern = /(\b(https?|ftp|file):\/\/|www\.)[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|]/ig;
 
@@ -51,8 +56,7 @@ const highlightLinkText = (text) => {
                 style="color: #007AFF; text-decoration: none;" 
                 onmouseover="this.style.color='blue'; this.style.textDecoration='underline';" 
                 onmouseout="this.style.color='#007AFF'; this.style.textDecoration='none';"
-              >
-              ${url}</a>`;
+              >${url}</a>`;
     }
   );
 };
@@ -61,14 +65,14 @@ const highlightLinkText = (text) => {
 const createHyperLink = (text) => {
   const markdownLinkPattern = /\[([^\]]+)\]\((https?:\/\/[^\s]+)\)/g;
   return text.replace(markdownLinkPattern, (match, label, url) => 
-      `<a href="${url}" target="_blank"
-          style="color: #007AFF; text-decoration: none;" 
-          onmouseover="this.style.color='blue'; this.style.textDecoration='underline';" 
-          onmouseout="this.style.color='#007AFF'; this.style.textDecoration='none';"
-        >
-        ${label}</a>`
+    `<a href="${url}" target="_blank"
+        style="color: #007AFF; text-decoration: none;" 
+        onmouseover="this.style.color='blue'; this.style.textDecoration='underline';" 
+        onmouseout="this.style.color='#007AFF'; this.style.textDecoration='none';"
+      >
+      ${label}</a>`
   );
-}
+};
 
 //(Router link)[/linkedin/LinkedinTips]
 const createRouterLink = (text) => {
@@ -80,28 +84,24 @@ const createRouterLink = (text) => {
                onmouseout="this.style.color='#007AFF'; this.style.textDecoration='none';"
             >${label}</a>`;
   });
-}
+};
 
 window.navigateToPath = (path) => {
   router.push(path);
-}
-
+};
 
 //@Image View@[{ 'name': 'ImageView', 'params': { 'Name': 'NSA Pic', 'Description': 'This is the NSA pic', 'Pic': 'seahawk.png'} }]
 const createRouterLinkWithProps = (text) => {
   const markdownPattern = /@([^@]+)@\[\s*({.*?})\s*\]/g;
-
   return text.replace(markdownPattern, (match, label, jsonString) => {
     let toProp;
-
     try {
       jsonString = jsonString.replace(/'/g, '"');
       toProp = JSON.parse(jsonString);
     } catch (e) {
       console.error('Error parsing JSON string:', e);
-      return match; 
+      return match;
     }
-
     return `<a href="javascript:void(0);" onclick="navigateToVuePath('${JSON.stringify(toProp).replace(/"/g, "&quot;")}')"
                style="color: #007AFF; text-decoration: none;" 
                onmouseover="this.style.color='blue'; this.style.textDecoration='underline';" 
@@ -109,44 +109,45 @@ const createRouterLinkWithProps = (text) => {
             >
             ${label}</a>`;
   });
-}
+};
 
 window.navigateToVuePath = (toPropString) => {
   const toProp = JSON.parse(toPropString.replace(/&quot;/g, "\""));
   router.push(toProp);
-}
+};
+
+const formatEntry = (value) => {
+  if (typeof value === 'string') {
+
+    if ((value.includes(' http://') || value.includes(' https://')) && !value.startsWith("[")){
+      return highlightLinkText(value);
+    }
+
+    value = createHyperLink(value);
+    value = createRouterLink(value);
+    value = createRouterLinkWithProps(value);
+  }
+
+  return value;
+};
 
 const filteredEntries = computed(() => {
   return Object.entries(item.value)
-          .filter(([key, value]) => value && shouldDisplay(key))
-          .map(([key, value]) => {
-
-            if (typeof value === 'string') {
-
-              if ((value.includes('http://') || value.includes('https://')) && !value.startsWith("[")){
-                value = highlightLinkText(value);
-              }
-
-              //[The link](https://www.target.com/)
-              value = createHyperLink(value);
-              //(Router link)[/linkedin/LinkedinTips]
-              value = createRouterLink(value);
-              //@Image View@[{ 'name': 'ImageView', 'params': { 'Name': 'NSA Pic', 'Description': 'This is the NSA pic', 'Pic': 'seahawk.png'} }]
-              value = createRouterLinkWithProps(value);
-            }
-
-            return [key, value];
-          });
+    .filter(([key, value]) => value && shouldDisplay(key))
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        // Format each element if value is an array
+        value = value.map(formatEntry);
+      } else {
+        // For string values, use the formatEntry method
+        value = formatEntry(value);
+      }
+      return [key, value];
+    });
 });
-
-// const formatKey = (key) => {
-//   return key.replace(/([A-Z])/g, ' $1').trim().replace(/^./, str => str.toUpperCase());
-// }
-
 </script>
 
 <style scoped>
-
 .accordion-menu {
   border: 1px solid #ccc;
   border-radius: 5px;
@@ -174,6 +175,15 @@ const filteredEntries = computed(() => {
   margin-top: 20px;
 }
 
+.menu-attribute {
+  margin-top: 6px;
+}
+
+.menu-items {
+  list-style-type: disc;
+  margin-left: 25px;
+}
+
 .accordion-content ul {
   padding: 0;
   list-style-type: none;
@@ -188,10 +198,4 @@ const filteredEntries = computed(() => {
   color: teal;
   font-size: 28px;
 }
-
 </style>
-
-
-
-
-  
