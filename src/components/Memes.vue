0ec20a803ba83a313dfd3221ad5d5f56f0470990
Michael Totaro <mtotaro@ncsu.edu>
2024-02-27 22:15:28 -0500

    Meme page rough draft

---
+++ src/components/Memes.vue
@@ -0,0 +1,86 @@
+<script>
+import Meme1 from "../assets/memes/RickAndMortySPMarine.jpg";
+import Meme2 from "../assets/memes/ScrollOfTruthSPM.jpg";
+import Meme3 from "../assets/memes/CopyTheseNotes.jpg";
+import Meme4 from "../assets/memes/GoogleBooks.jpg";
+import Meme5 from "../assets/memes/EightHoursAssembly.jpg";
+import Meme6 from "../assets/memes/ToManyIfStatements.jpg";
+import Meme7 from "../assets/memes/BugInTest.jpg";
+import Meme8 from "../assets/memes/PerfectDate.jpg";
+import Meme9 from "../assets/memes/PercentToPass.jpg";
+import Meme10 from "../assets/memes/BackendersDoingFrontEnd.jpg";
+import Meme11 from "../assets/memes/CloudyFriday.jpg";
+import Meme12 from "../assets/memes/CompSciMuscles.jpg";
+import Meme13 from "../assets/memes/InfiniteWhileLoop.jpg";
+import Meme14 from "../assets/memes/Database.jpg";
+import Meme15 from "../assets/memes/CommentOutCode.jpg";
+import Meme16 from "../assets/memes/BruteForceAttack.jpg";
+import Meme17 from "../assets/memes/PullStackDeveloper.jpg";
+import Meme18 from "../assets/memes/HelloWorld.jpg";
+import Meme19 from "../assets/memes/HurricaneFred.jpg";
+import Meme20 from "../assets/memes/StatusCodes.jpg";
+import Meme21 from "../assets/memes/PythonSpeed.jpg";
+import Meme22 from "../assets/memes/RTPEmployers.jpg";
+import Meme23 from "../assets/memes/ArtOfWar.jpg";
+import Meme24 from "../assets/memes/GravityFallsSpMarine.jpg";
+import Meme25 from "../assets/memes/ComfortLevels.jpg";
+import Meme26 from "../assets/memes/SomeFamousServers.jpg";
+import Meme27 from "../assets/memes/CrowdedShore.jpg";
+import Meme28 from "../assets/memes/React.jpg";
+import Meme29 from "../assets/memes/TransparentRight.jpg";
+import Meme30 from "../assets/memes/BitEightTimes.jpg";
+import Meme31 from "../assets/memes/AndroidStudioMeme.jpg";
+import Meme32 from "../assets/memes/SignIn.jpg";
+import Meme33 from "../assets/memes/GenZcode.jpg";
+import Meme34 from "../assets/memes/PageTwoGoogle.jpg";
+import Meme35 from "../assets/memes/DataStructureTeachers.jpg";
+import Meme36 from "../assets/memes/CrushKnowsBinary.jpg";
+import Meme37 from "../assets/memes/VerboseJava.jpg";
+import Meme38 from "../assets/memes/WhenYouVacation.jpg";
+import Meme39 from "../assets/memes/BobbyDropTables.jpg";
+import Meme40 from "../assets/memes/SendaCV.jpg";
+
+
+export const memeObjects = [
+    { text: "This is actual text", image: Meme1 }, //RickAndMortySPMarine.jpg
+    { text: "This is actual text", image: Meme2 }, //ScrollOfTruthSPM.jpg
+    { text: "This is actual text", image: Meme3 }, //CopyTheseNotes.jpg
+    { text: "This is actual text", image: Meme4 }, //GoogleBooks.jpg
+    { text: "This is actual text", image: Meme5 }, //EightHoursAssembly.jpg
+    { text: "This is actual text", image: Meme6 }, //ToManyIfStatements.jpg
+    { text: "This is actual text", image: Meme7 }, //BugInTest.jpg
+    { text: "This is actual text", image: Meme8 }, //PerfectDate.jpg
+    { text: "This is actual text", image: Meme9 }, //PercentToPass.jpg
+    { text: "This is actual text", image: Meme10 }, //BackendersDoingFrontEnd.jpg
+    { text: "This is actual text", image: Meme11 }, //CloudyFriday.jpg
+    { text: "This is actual text", image: Meme12 }, //CompSciMuscles.jpg
+    { text: "This is actual text", image: Meme13 }, //InfiniteWhileLoop.jpg
+    { text: "This is actual text", image: Meme14 }, //Database.jpg
+    { text: "This is actual text", image: Meme15 }, //CommentOutCode.jpg
+    { text: "This is actual text", image: Meme16 }, //BruteForceAttack.jpg
+    { text: "This is actual text", image: Meme17 }, //PullStackDeveloper.jpg
+    { text: "This is actual text", image: Meme18 }, //HelloWorld.jpg
+    { text: "This is actual text", image: Meme19 }, //HurricaneFred.jpg
+    { text: "This is actual text", image: Meme20 }, //StatusCodes.jpg
+    { text: "This is actual text", image: Meme21 }, //PythonSpeed.jpg
+    { text: "This is actual text", image: Meme22 }, //RTPEmployers.jpg
+    { text: "This is actual text", image: Meme23 }, //ArtOfWar.jpg
+    { text: "This is actual text", image: Meme24 }, //GravityFallsSpMarine.jpg
+    { text: "This is actual text", image: Meme25 }, //ComfortLevels.jpg
+    { text: "This is actual text", image: Meme26 }, //SomeFamousServers.jpg
+    { text: "This is actual text", image: Meme27 }, //CrowdedShore.jpg
+    { text: "This is actual text", image: Meme28 }, //React.jpg
+    { text: "This is actual text", image: Meme29 }, //TransparentRight.jpg
+    { text: "This is actual text", image: Meme30 }, //BitEightTimes.jpg
+    { text: "This is actual text", image: Meme31 }, //AndroidStudioMeme.jpg
+    { text: "This is actual text", image: Meme32 }, //SignIn.jpg
+    { text: "This is actual text", image: Meme33 }, //GenZcode.jpg
+    { text: "This is actual text", image: Meme34 }, //PageTwoGoogle.jpg
+    { text: "This is actual text", image: Meme35 }, //DataStructureTeachers.jpg
+    { text: "This is actual text", image: Meme36 }, //CrushKnowsBinary.jpg
+    { text: "This is actual text", image: Meme37 }, //VerboseJava.jpg
+    { text: "This is actual text", image: Meme38 }, //WhenYouVacation.jpg
+    { text: "This is actual text", image: Meme39 }, //BobbyDropTables.jpg
+    { text: "This is actual text", image: Meme40 } //SendaCV.jpg
+]
+</script>
