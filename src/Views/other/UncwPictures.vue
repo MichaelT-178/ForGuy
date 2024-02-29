03b2dee5d8ff45134e4f9f01b905306ace1ba918
Michael Totaro <mtotaro@ncsu.edu>
2024-02-28 23:57:11 -0500

    Added UNCW pics view

---
+++ src/Views/other/UncwPictures.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="uncw-pics-page">
+        <h1>UNCW Pictures</h1>
+        <p>Beautiful Pictures I took in Wilmington</p>
+        
+        <div class="uncw-pics-container" v-for="(pic, index) in uncwPics" :key="index">
+            <div class="uncw-pic-text">{{ index + 1 }}. {{ pic.text }}</div>
+            <img :src="pic.image" alt="Uncw Picture">
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { uncwPics } from "../../components/UncwPics.vue";
+
+</script>
+
+<style scoped>
+.uncw-pics-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
+.uncw-pics-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 400px;
+    margin-bottom: 35px;
+}
+
+.uncw-pic-text {
+    font-size: 18px;
+}
+
+img {
+    width: 400px;
+    border: 2px solid black;
+    margin-top: 8px;
+}
+
+p {
+    margin-top: -15px;
+    font-size: 20px;
+}
+
+</style>
---
+++ src/components/UncwPics.vue
@@ -0,0 +1,42 @@
+<script>
+import Pic1 from "../assets/UncwPics/SeptemberSunsetOne.jpg";
+import Pic2 from "../assets/UncwPics/PinkOverSeahawkLanding.jpg";
+import Pic3 from "../assets/UncwPics/PinkOverParkingLot.jpg";
+import Pic4 from "../assets/UncwPics/Geese.jpg";
+import Pic5 from "../assets/UncwPics/MooresvilleSunset.jpg";
+import Pic6 from "../assets/UncwPics/WeddingtonRainbow.jpg";
+import Pic7 from "../assets/UncwPics/SnowDay.jpg";
+import Pic8 from "../assets/UncwPics/HoggardHall.jpg";
+import Pic9 from "../assets/UncwPics/SunsetNearWagoner.jpg";
+import Pic10 from "../assets/UncwPics/RedSkyInTheDistance.jpg";
+import Pic11 from "../assets/UncwPics/CharlotteBoardwalkView.jpg";
+import Pic12 from "../assets/UncwPics/SeptemberSunsetTwo.jpg";
+import Pic13 from "../assets/UncwPics/OrangeNearSeahawkLanding.jpg";
+import Pic14 from "../assets/UncwPics/MarchSunset.jpg";
+import Pic15 from "../assets/UncwPics/UNCWTrail.jpg";
+import Pic16 from "../assets/UncwPics/TrailNearCourse.jpg";
+import Pic17 from "../assets/UncwPics/OrangeSkyOne.jpg";
+import Pic18 from "../assets/UncwPics/StudyRoom.jpg";
+
+export const uncwPics = [
+    { text: "Sunset in late September 2022", image: Pic1 }, //SeptemberSunsetOne.jpg
+    { text: "Pink sky over Seahawk Landing", image: Pic2 }, //PinkOverSeahawkLanding.jpg
+    { text: "Pink sky over the parking lot", image: Pic3 }, //PinkOverParkingLot.jpg
+    { text: "Geese!!", image: Pic4 }, //Geese.jpg
+    { text: "I think this was taken is Mooresville. Photo creds to my dad", image: Pic5 }, //MooresvilleSunset.jpg
+    { text: "Rainbow in Charlotte", image: Pic6 }, //WeddingtonRainbow.jpg
+    { text: "Snow day!! So white and fluffy 😍❄️☃️! (Jan. 2022)", image: Pic7 }, //SnowDay.jpg
+    { text: "Hoggard Hall in August 2021", image: Pic8 }, //HoggardHall.jpg
+    { text: "Orange Sky near Wagoner", image: Pic9 }, //SunsetNearWagoner.jpg
+    { text: "Red sky in the distance", image: Pic10 }, //RedSkyInTheDistance.jpg
+    { text: "The BoardWalk in Charlotte", image: Pic11 }, //CharlotteBoardwalkView.jpg
+    { text: "Another pic of this, because it was genuinely one of the craziest and most beautiful things I've ever seen", image: Pic12 }, //SeptemberSunsetTwo.jpg
+    { text: "Orange Sky over Cornerstone Hall(?)", image: Pic13 }, //OrangeNearSeahawkLanding.jpg
+    { text: "Pink Sky in late March near Terrapin Hall", image: Pic14 }, //MarchSunset.jpg
+    { text: "Off-campus trail in early April", image: Pic15 }, //UNCWTrail.jpg
+    { text: "Off-campus trail in late August near the rope climbing course", image: Pic16 }, //TrailNearCourse.jpg
+    { text: "Beautiful Orange Sky", image: Pic17 }, //OrangeSkyOne.jpg
+    { text: "Sunny Terrapin Study Room", image: Pic18 } //StudyRoom.jpg
+]
+
+</script>
