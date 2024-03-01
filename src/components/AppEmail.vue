c0e1ec90fc6c7b8c69ca1405e6656a68191083b1
Michael Totaro <mtotaro@ncsu.edu>
2024-03-01 17:18:17 -0500

    Completed AppsToDownload view

---
+++ src/components/AppEmail.vue
@@ -0,0 +1,40 @@
+<script>
+export const bodyContent = `Hi Elijah,%0A%0A` +
+`These are all the app links from my website. Enjoy.%0A%0A` +
+
+`Genuis Scan%0A` +
+`https://apps.apple.com/us/app/scanner-app-genius-scan/id377672876%0A%0A` +
+
+`LinkedIn%0A` +
+`https://apps.apple.com/us/app/linkedin-network-job-finder/id288429040%0A%0A` +
+
+`ChatGPT%0A` +
+`https://apps.apple.com/us/app/chatgpt/id6448311069%0A%0A` +
+
+`Alarmy%0A` +
+`https://apps.apple.com/us/app/alarmy-alarm-clock-sleep/id1163786766%0A%0A` +
+
+`Waze%0A` +
+`https://apps.apple.com/us/app/waze-navigation-live-traffic/id323229106%0A%0A` +
+
+`Microsoft Teams%0A` +
+`https://apps.apple.com/us/app/microsoft-teams/id1113153706%0A%0A` +
+
+`Uber%0A` +
+`https://apps.apple.com/us/app/uber-request-a-ride/id368677368%0A%0A` +
+
+`GrubHub%0A` +
+`https://apps.apple.com/us/app/grubhub-food-delivery/id302920553?ppid=9140aa46-ba99-49e6-834f-f3ae2844b70c%0A%0A` +
+
+`Canvas%0A` +
+`https://apps.apple.com/us/app/canvas-student/id480883488%0A%0A` +
+
+`Duo Mobile%0A` +
+`https://apps.apple.com/us/app/duo-mobile/id422663827%0A%0A` +
+
+`Microsoft Outlook%0A` +
+`https://apps.apple.com/us/app/microsoft-outlook/id951937596%0A%0A` +
+`Regards,%0A` +
+`Michael`;
+
+</script>
