67e9ee58cbdcddb8d71d3cd507dbbac2a904cff7
Michael Totaro <mtotaro@ncsu.edu>
2024-03-01 19:00:56 -0500

    Added skeleton of SearchResults.vue

---
+++ src/Views/SearchResults.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>Search results for: "{{ searchQuery }}"</p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, watch } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+
+  const searchQuery = ref(route.params.SearchQuery);
+  
+  watch(() => route.params.SearchQuery, (newSearchQuery) => {
+    searchQuery.value = newSearchQuery;
+  });
+  </script>
+  
+  
