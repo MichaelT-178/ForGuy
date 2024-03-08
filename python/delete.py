d3f61c2e696c0e57c408463872eceeaf610af6cc
Michael Totaro <mtotaro@ncsu.edu>
2024-03-08 13:57:22 -0500

    Completed ClassesIveTaken

--- python/delete.py
+++ python/delete.py
@@ -1,12 +1,28 @@
 import json 
 
-with open("../src/data/CompSci/VSCodeExtensions.json", "r") as file:
-    file_content = json.load(file)
 
+with open("../src/data/Classes/ClassesIveTaken.json", "r") as file:
+    content = json.load(file)
 
-extensions = file_content["Extensions"]
+freshman_classes = content["Freshman"]["First"]
+freshman_classes_two = content["Freshman"]["Second"]
+sophomore_classes = content["Sophomore"]["First"]
+sophomore_classes_two = content["Sophomore"]["Second"]
+junior_classes = content["Junior"]["First"]
+junior_classes_two = content["Junior"]["Second"]
+senior = content["Senior"]
 
-for extension in extensions:
-    print(f"`{extension['Name']}%0A` +")
-    print(f"`{extension['Link']}%0A%0A` +")
-    print()
+for a_class in freshman_classes:
+    print(a_class["Name"])
+for a_class in freshman_classes_two:
+    print(a_class["Name"])
+for a_class in sophomore_classes:
+    print(a_class["Name"])
+for a_class in sophomore_classes_two:
+    print(a_class["Name"])
+for a_class in junior_classes :
+    print(a_class["Name"])
+for a_class in junior_classes_two:
+    print(a_class["Name"])
+for a_class in senior:
+    print(a_class["Name"])
