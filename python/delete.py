3dee358d6915cd1a335838c49b176f92a3c87f4e
Michael Totaro <mtotaro@ncsu.edu>
2024-03-10 00:42:56 -0500

    Finished ClassesIveTaken and CSCDescriptions

--- python/delete.py
+++ python/delete.py
@@ -10,19 +10,34 @@ sophomore_classes = content["Sophomore"]["First"]
 sophomore_classes_two = content["Sophomore"]["Second"]
 junior_classes = content["Junior"]["First"]
 junior_classes_two = content["Junior"]["Second"]
-senior = content["Senior"]
+senior = content["Senior"]["First"]
+
+
+def print_json(json_data):
+    formatted_json = json.dumps(json_data, indent=4)
+    return formatted_json + ","
+
+
+
 
 for a_class in freshman_classes:
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
 for a_class in freshman_classes_two:
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
 for a_class in sophomore_classes:
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
 for a_class in sophomore_classes_two:
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
 for a_class in junior_classes :
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
 for a_class in junior_classes_two:
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
 for a_class in senior:
-    print(a_class["Name"])
+    if "CSC" in a_class["Name"].upper():
+        print(print_json(a_class))
