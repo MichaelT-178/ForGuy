5a2c5048bef4766d15ea3d4bcb093d11491a42b2
Michael Totaro <mtotaro@ncsu.edu>
2024-03-18 11:01:30 -0400

    Updated python search data

--- python/create_search_data.py
+++ python/create_search_data.py
@@ -45,112 +45,20 @@ home_str = ('def get_home():\n'
 
 all_functions_str += home_str
 
-print(all_functions_str.strip())
+func_vars = []
 
+for path in paths:
 
-def get_classestoavoid():
-    with open("../src/data/Classes/ClassesToAvoid.json", "r") as file:
-        content = json.load(file)
+    for file_name in get_files_in_directory(path):
+        file = file_name.replace(".json", "")
+        
+        if ".docx" not in file_name and ".vue" not in file_name:
+            func_call_str = f'{file.lower()} = get_{file.lower()}()\n'
+            func_vars.append(file.lower())
 
-def get_classrecommendations():
-    with open("../src/data/Classes/ClassRecommendations.json", "r") as file:
-        content = json.load(file)
+        all_functions_str += func_call_str
 
-def get_usefulcertifications():
-    with open("../src/data/Classes/UsefulCertifications.json", "r") as file:
-        content = json.load(file)
+all_functions_str = all_functions_str.strip()
 
-def get_cscdescriptions():
-    with open("../src/data/Classes/CSCDescriptions.json", "r") as file:
-        content = json.load(file)
-
-def get_classesivetaken():
-    with open("../src/data/Classes/ClassesIveTaken.json", "r") as file:
-        content = json.load(file)
-
-def get_classesivetaken():
-    with open("../src/data/Classes/ClassesIveTaken.json", "r") as file:
-        content = json.load(file)
-
-def get_vscodeshortcuts():
-    with open("../src/data/CompSci/VSCodeShortcuts.json", "r") as file:
-        content = json.load(file)
-
-def get_uiux_tips():
-    with open("../src/data/CompSci/UIUX_Tips.json", "r") as file:
-        content = json.load(file)
-
-def get_vscodeextensions():
-    with open("../src/data/CompSci/VSCodeExtensions.json", "r") as file:
-        content = json.load(file)
-
-def get_offersreceived():
-    with open("../src/data/jobs/OffersReceived.json", "r") as file:
-        content = json.load(file)
-
-def get_howtogetajob():
-    with open("../src/data/jobs/HowToGetAJob.json", "r") as file:
-        content = json.load(file)
-
-def get_howtogetajob():
-    with open("../src/data/jobs/HowToGetAJob.json", "r") as file:
-        content = json.load(file)
-
-def get_interviewtips():
-    with open("../src/data/jobs/InterviewTips.json", "r") as file:
-        content = json.load(file)
-
-def get_wheretoapply():
-    with open("../src/data/jobs/WhereToApply.json", "r") as file:
-        content = json.load(file)
-
-def get_careerpathadvice():
-    with open("../src/data/jobs/CareerPathAdvice.json", "r") as file:
-        content = json.load(file)
-
-def get_resumetemplatetext():
-    with open("../src/data/jobs/ResumeTemplateText.json", "r") as file:
-        content = json.load(file)
-
-def get_whatislinkedin():
-    with open("../src/data/LinkedIn/WhatIsLinkedIn.json", "r") as file:
-        content = json.load(file)
-
-def get_linkedintips():
-    with open("../src/data/LinkedIn/LinkedInTips.json", "r") as file:
-        content = json.load(file)
-
-def get_setupprofile():
-    with open("../src/data/LinkedIn/SetupProfile.json", "r") as file:
-        content = json.load(file)
-
-def get_setupjobalerts():
-    with open("../src/data/LinkedIn/SetupJobAlerts.json", "r") as file:
-        content = json.load(file)
-
-def get_contacts():
-    with open("../src/data/LinkedIn/Contacts.json", "r") as file:
-        content = json.load(file)
-
-def get_keyboardshortcuts():
-    with open("../src/data/Other/KeyboardShortcuts.json", "r") as file:
-        content = json.load(file)
-
-def get_appstodownload():
-    with open("../src/data/Other/AppsToDownload.json", "r") as file:
-        content = json.load(file)
-
-def get_terminalcommands():
-    with open("../src/data/Other/TerminalCommands.json", "r") as file:
-        content = json.load(file)
-
-def get_generaltips():
-    with open("../src/data/Other/GeneralTips.json", "r") as file:
-        content = json.load(file)
-
-def get_home():
-    with open("../src/data/Home.json", "r") as file:
-        content = json.load(file)
-
-
-
+print(all_functions_str)
+print(func_vars)
