0bcdeaeb7e10789cd9dfea664075def979a12163
Michael Totaro <mtotaro@ncsu.edu>
2024-03-20 00:31:48 -0400

    Added code Block draft

--- src/Views/other/CsStuffILike.vue
+++ src/Views/other/CsStuffILike.vue
@@ -2,5 +2,21 @@
     <main class="about-page">
         <h1>CS Stuff I like</h1>
         <p>This is the CS Stuff I like page</p>
+
+        <div v-for="code in codeSyntax" :key="code.Name">
+            <CodeBlock :codeInfo="code"></CodeBlock>
+        </div>
     </main>
 </template>
+
+
+<script setup>
+import { ref } from 'vue';
+import AllData from '../../data/Other/FavoriteCsStuff.json'
+import CodeBlock from '../../components/Code/CodeBlock.vue'
+
+
+const jsonData = ref(AllData);
+const codeSyntax = jsonData.value["Code"];
+
+</script>
