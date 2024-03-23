d13111a6752f5571812b0a1f5db410eb79a570f7
Michael Totaro <mtotaro@ncsu.edu>
2024-03-23 13:45:49 -0400

    Updated Code blocks

--- src/components/Code/CodeBlock.vue
+++ src/components/Code/CodeBlock.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <h2>{{ codeInfo.Name }}</h2>
-    <p>{{ codeInfo.Description }}</p>
+  <div class="container">
+    <div class="text-container">
+      <h2 class="code-name">{{ codeInfo.Name }}</h2>
+      <p class="description">{{ codeInfo.Description }}</p>
+    </div>
     <div class="code-block">
       <div class="copy-bar">
-        <span class="language-name">{{ codeInfo.Language }}</span>
+        <span class="language-name">{{ codeInfo.Language ?? "Command"}}</span>
         <span class="material-icons copy-icon" 
              :class="{'icon-done': copyIcon === 'done'}" 
              :style="{ color: copyIcon === 'done' ? '#00FF4D' : getLanguageColor }"
@@ -15,6 +17,7 @@
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref, computed } from 'vue';
@@ -34,7 +37,8 @@ const props = defineProps({
       Name: "",
       Description: "",
       Language: "",
-      Code: ""
+      FormatCode: "",
+      CopyCode :""
     }),
     required: true,
   },
@@ -43,17 +47,16 @@ const props = defineProps({
 const copyIcon = ref('content_copy');
 
 const copyCode = () => {
-  navigator.clipboard.writeText(props.codeInfo.Code)
+  navigator.clipboard.writeText(props.codeInfo.CopyCode)
     .then(() => {
       copyIcon.value = 'done';
-      setTimeout(() => copyIcon.value = 'content_copy', 2000); // Resets icon after delay
+      setTimeout(() => copyIcon.value = 'content_copy', 2000);
     })
     .catch(err => {
-      // Handle error, if clipboard access failed
+      console.error("Failed to copy code to clipboard! ", err)
     });
 };
 
-// Extending Prism for custom syntax highlighting
 Prism.languages.python = Prism.languages.extend('python', {
   'decorator': {
     pattern: /\bprint\b(?=\()/,
@@ -68,36 +71,69 @@ Prism.languages.python = Prism.languages.extend('python', {
 });
 
 const highlightedCode = computed(() => {
+  if (props.codeInfo.Language === null) {
+    return `<span style="color: #C1C1C1;">${props.codeInfo.FormatCode}</span>`;
+  }
+
   const language = props.codeInfo.Language.toLowerCase();
   const prismLanguage = Prism.languages[language] || Prism.languages.plain;
-  return Prism.highlight(props.codeInfo.Code, prismLanguage, language);
+  return Prism.highlight(props.codeInfo.FormatCode, prismLanguage, language);
 });
 
 const getLanguageColor = computed(() => {
-  switch (props.codeInfo.Language.toLowerCase().trim()) {
-    case 'python': return '#3572A5';
-    case 'java': return '#b07219';
-    case 'javascript': return '#f1e05a';
-    case 'typescript': return '#3178c6';
-    case 'swift': return '#F05138';
-    case 'ruby': return '#701516';
-    case 'vue': return '#41b883';
-    case 'json': return '#FF9A00';
-    default: return '#D5D5D5';
+
+  //I prefer this approach over a switch statement
+  const colors = {
+    python: '#3572A5',
+    java: '#C6821E',
+    javascript: '#f1e05a',
+    typescript: '#3178c6',
+    swift: '#F05138',
+    ruby: '#701516',
+    vue: '#41b883',
+    json: '#FF9A00'
   }
+
+  if (props.codeInfo.Language === null) return "#DFC200";
+
+  const color = props.codeInfo.Language.toLowerCase().trim();
+
+  return colors[color]
+
 });
 
 </script>
 
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.text-container {
+  max-width: 500px; 
+}
+
+.code-name {
+  margin-left: 4px;
+  font-size: 23px;
+}
+
+.description {
+  margin-top: -15px;
+  margin-left: 4px;
+}
+
 .code-block {
-  margin: 20px;
+  margin: 20px 0;
   border: 1px solid #ddd;
   border-radius: 5px;
-  background-color: #282C34; /* Changed to black */
-  color: white; /* Ensures text is white */
+  background-color: #282C34;
+  color: white;
   width: 500px;
+  margin-top: -5px;
 }
 
 .language-name {
@@ -106,14 +142,12 @@ const getLanguageColor = computed(() => {
 }
 
 .copy-icon {
-  /* color: #868686; */
-  /* color: #CDB100; */
   cursor: pointer;
   font-size: 22px;
 }
 
 .copy-icon:hover {
-  filter: brightness(80%); /* Darkens the icon on hover */
+  filter: brightness(80%);
 }
 
 .icon-done {
@@ -121,28 +155,28 @@ const getLanguageColor = computed(() => {
 }
 
 .icon-done:hover {
-  color: #00FF4D; /* Keeps the icon green when hovered over in 'done' state */
+  color: #00FF4D;
 }
 
 .copy-bar {
   display: flex;
-  justify-content: space-between; /* Adjusted for space-between to align items properly */
+  justify-content: space-between;
   background-color: gray;
   padding: 5px;
 }
 
 .language-name {
-  flex-grow: 1; /* Ensures language name aligns to the left */
+  flex-grow: 1;
   font-size: 20px;
   margin-left: 6px;
 }
 
 code {
-  background-color: #282C34; /* Background set to black */
-  /* padding: 5px; */
+  background-color: #282C34;
   border-radius: 3px;
   font-size: 13.5px;
   margin-left: 16px;
   line-height: 20px;
 }
+
 </style>
