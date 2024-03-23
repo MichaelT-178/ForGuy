f199e465498842a2678be3d641a12d7fb4b61801
Michael Totaro <mtotaro@ncsu.edu>
2024-03-23 13:45:49 -0400

    Updated Code blocks

--- src/Views/other/CsStuffILike.vue
+++ src/Views/other/CsStuffILike.vue
@@ -3,7 +3,7 @@
         <h1>CS Stuff I like</h1>
         <p>This is the CS Stuff I like page</p>
 
-        <div v-for="code in codeSyntax" :key="code.Name">
+        <div v-for="code in codeSyntax" :key="code.Name" class="code-blocks">
             <CodeBlock :codeInfo="code"></CodeBlock>
         </div>
     </main>
@@ -20,3 +20,10 @@ const jsonData = ref(AllData);
 const codeSyntax = jsonData.value["Code"];
 
 </script>
+
+<style scoped>
+.code-blocks {
+    margin-left: 100px;
+}
+
+</style>
