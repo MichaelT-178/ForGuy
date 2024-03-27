2bbe5198758ab000df2a62404eb733bae4daa368
Michael Totaro <mtotaro@ncsu.edu>
2024-03-27 10:27:01 -0400

    Updated highlight syntax

---
+++ src/unused/OldPrismHighlight.vue
@@ -0,0 +1,8 @@
+// Prism.languages.python = Prism.languages.extend('python', {
+//   'decorator': {
+//     pattern: /\bprint\b(?=\()/,
+//     lookbehind: true,
+//     alias: 'keyword'
+//   }
+// });
+    
