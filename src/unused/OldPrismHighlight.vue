f322a9201ff19fbf7d84f77089dc86461975eedd
Michael Totaro <mtotaro@ncsu.edu>
2024-04-02 21:58:29 -0400

    Updated spelling

--- src/unused/OldPrismHighlight.vue
+++
@@ -1,8 +0,0 @@
-// Prism.languages.python = Prism.languages.extend('python', {
-//   'decorator': {
-//     pattern: /\bprint\b(?=\()/,
-//     lookbehind: true,
-//     alias: 'keyword'
-//   }
-// });
-    
