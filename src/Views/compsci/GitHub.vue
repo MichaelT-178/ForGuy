66572e6b5d4aa4c8752135a46b39ea4a110a8bdb
Michael Totaro <mtotaro@ncsu.edu>
2024-04-04 00:08:15 -0400

    Scroll function working

--- src/Views/compsci/GitHub.vue
+++ src/Views/compsci/GitHub.vue
@@ -3,11 +3,28 @@
         <h1 class="gh-header">GitHub</h1>
         <p class="description">This is a description of GitHub and common commands.</p>
         <img src="../../assets/GitHub.png" alt="GitHub" class="github-pic"/>
+
+
+
         <h2 class="gh-header-two">What is GitHub and Git?</h2>
         <p class="description-two">How to get an existing folder onto GitHub.</p>
 
-        <h2 class="gh-header-two">Existing Folder Commands</h2>
+
+
+        <h2 class="gh-header-two">How to Create a GitHub Account</h2>
+        <p class="description-two">These are instructions to setup Git and create a GitHub account.</p>
+
+        <div v-for="point in setupGitHub" :key="point.id">
+            <p class="bullet-pt"><span class="bullet-pt-span">{{ point.id }}
+                </span><span v-html="createHyperLink(processedTipContent(point.instruction))"></span></p>
+            <span v-if="point.Code"><CodeBlock :codeInfo="point.Code" style="margin-bottom: 20px;"></CodeBlock></span>
+        </div>
+
+
+
+        <h2 class="gh-header-two" ref="codeBlockRef">Existing Folder Commands</h2>
         <p class="description-two">How to get an existing folder onto GitHub.</p>
+
 
         <div v-for="point in existingFolderPts" :key="point.id">
             <p class="bullet-pt"><span class="bullet-pt-span">{{ point.id }}</span><span v-html="createHyperLink(point.instruction)"></span></p>
@@ -18,15 +35,70 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import AllData from '../../data/CompSci/GitHub.json';
 import CmdTable from "../../components/CommandTable.vue";
 import { createHyperLink } from "../../components/FormatLinks.vue";
+import CodeBlock from '../../components/Code/CodeBlock.vue';
+
 
 const jsonData = ref(AllData);
 
+
+
+const setupGitHub = jsonData.value["SetupGitHub"];
+
+
 const existingFolderPts = jsonData.value["ExistingFolderPts"];
 const existingFolderCmds = jsonData.value["ExistingFolder"];
+
+
+
+
+
+//Add refs here
+const codeBlockRef = ref(null);
+// const codeBlockTwoRef = ref(null);
+// const codeBlockThreeRef = ref(null);
+// const codeBlockFourRef = ref(null);
+
+
+const scrollToRef = (refName) => {
+
+    
+    //Add refs here
+    const refs = {
+        codeBlockRef,
+        // codeBlockTwoRef,
+        // codeBlockThreeRef,
+        // codeBlockFourRef
+    };
+
+    const targetElement = refs[refName]?.value;
+
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.warn("No target element found for refName:", refName);
+    }
+}
+
+const processedTipContent = (tip) => {
+    return tip.replace(/Scroll down\((.*?)\)/g, (match, refName) => {
+        return `<span href="#" class="scroll-down" data-ref-name="${refName}">Scroll down</span>`;
+    });
+}
+
+onMounted(() => {
+    document.querySelectorAll('.scroll-down').forEach(element => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+            const refName = event.target.getAttribute('data-ref-name');
+            scrollToRef(refName);
+        });
+    });
+});
+
 
 </script>
 
@@ -80,6 +152,17 @@ const existingFolderCmds = jsonData.value["ExistingFolder"];
     margin-bottom: 24px;
     font-size: 19px;
     width: 690px;
+}
+
+:deep(.scroll-down) {
+    text-decoration: none;
+    color: blue;
+    cursor: pointer;
+}
+
+:deep(.scroll-down:hover) {
+    text-decoration: underline;
+    color: darkblue;
 }
 
 .bullet-pt-span {
