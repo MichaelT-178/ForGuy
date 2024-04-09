2360a7690242387aca1195e4be408b39dbaf3f68
Michael Totaro <mtotaro@ncsu.edu>
2024-04-09 17:28:11 -0400

    Added data

--- src/Views/other/CsStuffILike.vue
+++ src/Views/other/CsStuffILike.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
-        <h1>CS Stuff I like</h1>
-        <p>This is the CS Stuff I like page. Note the them being used for the code blocks is Atom Dark One.</p>
+        <h1 class="gh-header">CS Stuff I like</h1>
+        <p class="description">These are various computer science and programming-related things I like.</p>
 
-        <h2 class="gh-header-two" ref="createGHAccount">How to Create a GitHub Account</h2>
-        <p class="description-two">These are instructions to setup Git and create a GitHub account.</p>
+        <h2 class="gh-header-two">Comp Sci Stuff I like</h2>
+        <p class="description-two">These are various computer science related things I like.</p>
 
         <div v-for="point in csStuff" :key="point.id">
             <p class="bullet-pt"><span class="bullet-pt-span">{{ point.id }}
@@ -12,9 +12,15 @@
             </p>
         </div>
 
+        <h1 class="header-three">Goated Syntax</h1>
+        <p class="description-three">These are code snippets that show syntax and functionality I like.</p>
+
         <div v-for="code in codeSyntax" :key="code.Name" class="code-blocks">
             <CodeBlock :codeInfo="code"></CodeBlock>
         </div>
+
+        <p class="scroll-up-btn" @click="scrollToTop">Scroll To Top</p>
+
     </div>
 </template>
 
@@ -25,13 +31,17 @@ import AllData from '../../data/Other/CsStuffILike.json';
 import CodeBlock from '../../components/Code/CodeBlock.vue';
 import { createHyperLink } from "../../components/FormatLinks.vue";
 
-
 const jsonData = ref(AllData);
 
 const csStuff = jsonData.value["CsStuff"];
-
-
 const codeSyntax = jsonData.value["Code"];
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
 
 </script>
 
@@ -42,6 +52,19 @@ const codeSyntax = jsonData.value["Code"];
     flex-direction: column;
     align-items: center;
     width: 100%;
+}
+
+.gh-header {
+    text-align: left;
+    border-bottom: 1.5px solid #d8dee4;
+    padding-bottom: 7px;
+    width: 690px;
+}
+
+.description {
+    margin-top: -8px;
+    font-size: 19px;
+    width: 690px;
 }
 
 .bullet-pt {
@@ -81,7 +104,43 @@ const codeSyntax = jsonData.value["Code"];
     width: 690px;
 }
 
+
+.header-three {
+    margin-top: 40px;
+    margin-bottom: -10px;
+}
+
+.description-three {
+    /* margin-top: -15px; */
+    font-size: 21px;
+    margin-bottom: 10px;
+}
+
+.scroll-up-btn {
+    text-align: center; 
+    font-size: 23.5px; 
+    margin-top: 25px;
+    margin-bottom: 75px;
+    margin-right: 40px;
+    cursor: pointer;
+    color: blue;
+}
+
+.scroll-up-btn:hover {
+    color: darkblue;
+    text-decoration: underline;
+}
+
 @media (max-width: 700px) {
+
+    .gh-header {
+        width: 490px;
+    }
+    
+    .description {
+        width: 490px;
+    }
+    
     .gh-header-two {
         width: 490px;
     }
