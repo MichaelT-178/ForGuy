a835cf4fee76e4f01fab8494fbdad246f97b72c9
Michael Totaro <mtotaro@ncsu.edu>
2024-04-18 18:04:44 -0400

    Updated comments

--- src/components/SearchBarTwo.vue
+++ src/components/SearchBarTwo.vue
@@ -33,145 +33,135 @@
           <div class="result-item" @click="goToResultsPage">
             Press <span style="color: #7A008D">return/enter</span> to see results for "{{ searchQuery }}"
           </div>
-        </div>
-        
-      </div>
+        </div> 
     </div>
-  </template>
+  </div>
+</template>
   
+
+<script setup>
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import SearchPages from '../data/SearchPages.json'; 
   
-  <script setup>
-  import { ref, watch } from 'vue';
-  import { useRouter } from 'vue-router';
-  import SearchPages from '../data/SearchPages.json'; 
-  
-  const searchQuery = ref('');
-  const filteredResults = ref([]);
-  const emit = defineEmits(['close-search']);
-  const router = useRouter();
-  
-  const goToResultsPage = () => {
-    emit('close-search');
-    router.push({ name: 'SearchResults', params: { SearchQuery: searchQuery.value } });
-  };
-  
-  const handleEnterPress = (event) => {
-    if (event.key === 'Enter') {
-      goToResultsPage();
-    }
-  };
-  
-  const filterResults = () => {
-    if (searchQuery.value) {
-      filteredResults.value = SearchPages.filter((item) =>
-        item.MenuName.toLowerCase().includes(searchQuery.value.toLowerCase())
-      ).slice(0, 11); //Only 11 results show at a time.
-    } else {
-      filteredResults.value = [];
-    }
-  };
-  
-  const closeSearchBar = () => {
-    emit('close-search');
-  };
-  
-  watch(searchQuery, filterResults);
-  
-  </script>
-  
-  
-  <style scoped>
-  
-  .search-bar-container {
-    position: relative;
-    width: 100%;
-    background-color: white;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 2;
+const searchQuery = ref('');
+const filteredResults = ref([]);
+const emit = defineEmits(['close-search']);
+const router = useRouter();
+
+const goToResultsPage = () => {
+  emit('close-search');
+  router.push({ name: 'SearchResults', params: { SearchQuery: searchQuery.value } });
+};
+
+const handleEnterPress = (event) => {
+  if (event.key === 'Enter') {
+    goToResultsPage();
   }
+};
   
-  .input-icon-container {
-    position: relative;
-    display: flex;
-    align-items: center;
-    width: 100%;
+const filterResults = () => {
+  if (searchQuery.value) {
+    filteredResults.value = SearchPages.filter((item) =>
+      item.MenuName.toLowerCase().includes(searchQuery.value.toLowerCase())
+    ).slice(0, 11); //Only 11 results show at a time.
+  } else {
+    filteredResults.value = [];
   }
+};
+
+const closeSearchBar = () => {
+  emit('close-search');
+};
+
+watch(searchQuery, filterResults);
   
-  .material-icons {
-    position: absolute;
-    padding: 10px;
-    color: #757575;
-    pointer-events: none;
-  }
+</script>
   
-  .search-bar-container input[type="text"] {
-    width: 100%;
-    height: 36px;
-    padding: 0 15px;
-    padding-left: 48px;
-    font-size: 16px;
-    border: none;
-    background-color: #F9F9F9;
-  }
   
-  .results {
-    position: absolute;
-    top: 100%;
-    width: 100%;
-    background-color: white;
-    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
-    z-index: 100;
-    overflow: hidden;
-    background-color: #00C0C0;
-  }
+<style scoped>
+
+.search-bar-container {
+  position: relative;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 2;
+}
+
+.input-icon-container {
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
   
-  .emoji {
-    font-size: 19px;
-    margin-right: 4.85px;
-  }
+.material-icons {
+  position: absolute;
+  padding: 10px;
+  color: #757575;
+  pointer-events: none;
+}
+
+.search-bar-container input[type="text"] {
+  width: 100%;
+  height: 36px;
+  padding: 0 15px;
+  padding-left: 48px;
+  font-size: 16px;
+  border: none;
+  background-color: #F9F9F9;
+}
+
+.results {
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  z-index: 100;
+  overflow: hidden;
+  background-color: #00C0C0;
+}
   
-  .result-item {
-    display: block;
-    padding: 10px 15px;
-    color: black;
-    text-decoration: none;
-    text-align: left;
-    line-height: 1; 
-  }
+.emoji {
+  font-size: 19px;
+  margin-right: 4.85px;
+}
+
+.result-item {
+  display: block;
+  padding: 10px 15px;
+  color: black;
+  text-decoration: none;
+  text-align: left;
+  line-height: 1; 
+}
   
-  .result-item:hover {
-    background-color: #ffd483;
-    color: #333;
-    cursor: pointer;
-  }
+.result-item:hover {
+  background-color: #ffd483;
+  color: #333;
+  cursor: pointer;
+}
+
+.rectangular {
+  border-radius: 0;
+}
+
+#close-icon {
+  position: absolute;
+  cursor: pointer; 
+  pointer-events: auto; 
+  right: 4.75px; 
+  color: #757575; 
+  padding: 10px;
+  font-size: 23.5px;
+}
+
+#close-icon:hover {
+  color: #B91212;
+}
   
-  .rectangular {
-    border-radius: 0;
-  }
-  
-  /* .rounded-all {
-    border-radius: 19px;
-  } */
-  
-  #close-icon {
-    position: absolute;
-    cursor: pointer; 
-    pointer-events: auto; 
-    right: 4.75px; 
-    color: #757575; 
-    padding: 10px;
-    font-size: 23.5px;
-  }
-  
-  #close-icon:hover {
-    color: #B91212;
-  }
-  /* @media (max-width: 900px) {
-    .search-bar-container {
-      max-width: 400px;
-    }
-  } */
-  
-  </style>
+</style>
   
     
