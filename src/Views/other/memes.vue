417554ff9578fbfe4f9fc921f72784230a6a73cb
Michael Totaro <mtotaro@ncsu.edu>
2024-04-20 02:48:04 -0400

    Fixed meme view

--- src/Views/other/memes.vue
+++ src/Views/other/memes.vue
@@ -5,8 +5,10 @@
         
         <div class="meme-container" v-for="(meme, index) in memeObjects" :key="index">
             <div class="meme-text">{{ index + 1 }}. {{ meme.text }}</div>
-            <img :src="meme.image" alt="Meme Picture">
-            <span class="hidden-text">{{ meme.transcription }}</span>
+            <div class="image-container">
+                <img :src="meme.image" alt="Meme Picture">
+                <span class="hidden-text">{{ meme.transcription }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -43,26 +45,33 @@ const text = jsonData.value["Text"][0];
     font-size: 18px;
 }
 
+.image-container {
+    position: relative;
+    width: 400px;
+}
+
 .hidden-text {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  padding: 0;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    pointer-events: none;
+    color: transparent;
+    background: transparent;
+    border: none;
 }
 
 img {
-    width: 400px;
+    width: 100%;
     border: 2px solid black;
     margin-top: 8px;
+    display: block;
 }
 
 p {
     margin-top: -15px;
     font-size: 20px;
 }
-
 </style>
