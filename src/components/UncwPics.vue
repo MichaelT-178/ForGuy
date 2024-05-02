920c0ec1d3e835463f466bd2d1e001a9480ba1b7
Michael Totaro <mtotaro@ncsu.edu>
2024-05-02 01:12:02 -0400

    Added data

--- src/components/UncwPics.vue
+++ src/components/UncwPics.vue
@@ -2,60 +2,64 @@
 import Pic1 from "../assets/UncwPics/SeptemberSunsetOne.jpg";
 import Pic2 from "../assets/UncwPics/PinkOverSeahawkLanding.jpg";
 import Pic3 from "../assets/UncwPics/PinkOverParkingLot.jpg";
-import Pic4 from "../assets/UncwPics/Geese.jpg";
-import Pic5 from "../assets/UncwPics/SnowDay.jpg";
-import Pic6 from "../assets/UncwPics/HoggardHall.jpg";
-import Pic7 from "../assets/UncwPics/SunsetNearWagoner.jpg";
-import Pic8 from "../assets/UncwPics/RedSkyInTheDistance.jpg";
-import Pic9 from "../assets/UncwPics/SeptemberSunsetTwo.jpg";
-import Pic10 from "../assets/UncwPics/OrangeNearSeahawkLanding.jpg";
-import Pic11 from "../assets/UncwPics/MarchSunset.jpg";
-import Pic12 from "../assets/UncwPics/UNCWTrail.jpg";
-import Pic13 from "../assets/UncwPics/TrailNearCourse.jpg";
-import Pic14 from "../assets/UncwPics/OrangeSkyOne.jpg";
-import Pic15 from "../assets/UncwPics/StudyRoom.jpg";
-import Pic16 from "../assets/UncwPics/MooresvilleSunset.jpg";
-import Pic17 from "../assets/UncwPics/WeddingtonRainbow.jpg";
-import Pic18 from "../assets/UncwPics/CharlotteBoardwalkView.jpg";
-import Pic19 from "../assets/UncwPics/SunriseInDurham.jpg";
-import Pic20 from "../assets/UncwPics/PinkSkyMarchDurham.jpg";
-import Pic21 from "../assets/UncwPics/RedSunsetMarch.jpg";
-import Pic22 from "../assets/UncwPics/DurhamSidewalkSunset.jpg";
-import Pic23 from "../assets/UncwPics/FireyClouds.jpg";
-import Pic24 from "../assets/UncwPics/RollingClouds.jpg";
-import Pic25 from "../assets/UncwPics/Texas.jpg";
+import Pic4 from "../assets/UncwPics/FirstDayClass.jpg";
+import Pic5 from "../assets/UncwPics/Geese.jpg";
+import Pic6 from "../assets/UncwPics/SnowDay.jpg";
+import Pic7 from "../assets/UncwPics/HoggardHall.jpg";
+import Pic8 from "../assets/UncwPics/SunsetNearWagoner.jpg";
+import Pic9 from "../assets/UncwPics/RedSkyInTheDistance.jpg";
+import Pic10 from "../assets/UncwPics/SeptemberSunsetTwo.jpg";
+import Pic11 from "../assets/UncwPics/OrangeNearSeahawkLanding.jpg";
+import Pic12 from "../assets/UncwPics/MarchSunset.jpg";
+import Pic13 from "../assets/UncwPics/UNCWTrail.jpg";
+import Pic14 from "../assets/UncwPics/TrailNearCourse.jpg";
+import Pic15 from "../assets/UncwPics/OrangeSkyOne.jpg";
+import Pic16 from "../assets/UncwPics/Theater.jpg";
+import Pic17 from "../assets/UncwPics/StudyRoom.jpg";
+import Pic18 from "../assets/UncwPics/MooresvilleSunset.jpg";
+import Pic19 from "../assets/UncwPics/WeddingtonRainbow.jpg";
+import Pic20 from "../assets/UncwPics/CharlotteBoardwalkView.jpg";
+import Pic21 from "../assets/UncwPics/SunriseInDurham.jpg";
+import Pic22 from "../assets/UncwPics/PinkSkyMarchDurham.jpg";
+import Pic23 from "../assets/UncwPics/RedSunsetMarch.jpg";
+import Pic24 from "../assets/UncwPics/DurhamSidewalkSunset.jpg";
+import Pic25 from "../assets/UncwPics/FireyClouds.jpg";
+import Pic26 from "../assets/UncwPics/RollingClouds.jpg";
+import Pic27 from "../assets/UncwPics/Texas.jpg";
 
 
 export const uncwPics = [
     { text: "Sunset in late September 2022", image: Pic1 }, //SeptemberSunsetOne.jpg
     { text: "Pink sky over Seahawk Landing", image: Pic2 }, //PinkOverSeahawkLanding.jpg
     { text: "Pink sky over the parking lot", image: Pic3 }, //PinkOverParkingLot.jpg
-    { text: "Geese!!", image: Pic4 }, //Geese.jpg
-    { text: "Snow day!! So white and fluffy 😍❄️☃️! (Jan. 2022)", image: Pic5 }, //SnowDay.jpg
-    { text: "Hoggard Hall in August 2021", image: Pic6 }, //HoggardHall.jpg
-    { text: "Orange Sky near Wagoner", image: Pic7 }, //SunsetNearWagoner.jpg
-    { text: "Red sky in the distance", image: Pic8 }, //RedSkyInTheDistance.jpg
-    { text: "Another pic of this, because it was genuinely one of the craziest and most beautiful things I've ever seen", image: Pic9 }, //SeptemberSunsetTwo.jpg
-    { text: "Orange Sky over Cornerstone Hall(?)", image: Pic10 }, //OrangeNearSeahawkLanding.jpg
-    { text: "Pink Sky in late March near Terrapin Hall", image: Pic11 }, //MarchSunset.jpg
-    { text: "Off-campus trail in early April", image: Pic12 }, //UNCWTrail.jpg
-    { text: "Off-campus trail in late August near the rope climbing course", image: Pic13 }, //TrailNearCourse.jpg
-    { text: "Beautiful Orange Sky", image: Pic14 }, //OrangeSkyOne.jpg
-    { text: "Sunny Terrapin Study Room", image: Pic15 } //StudyRoom.jpg
+    { text: "First day of Class on Campus 2021", image: Pic4 }, //FirstDayClass.jpg
+    { text: "Geese!!", image: Pic5 }, //Geese.jpg
+    { text: "Snow day!! So white and fluffy 😍❄️☃️! (Jan. 2022)", image: Pic6 }, //SnowDay.jpg
+    { text: "Hoggard Hall in August 2021", image: Pic7 }, //HoggardHall.jpg
+    { text: "Orange Sky near Wagoner", image: Pic8 }, //SunsetNearWagoner.jpg
+    { text: "Red sky in the distance", image: Pic9 }, //RedSkyInTheDistance.jpg
+    { text: "Another pic of this, because it was genuinely one of the craziest and most beautiful things I've ever seen", image: Pic10 }, //SeptemberSunsetTwo.jpg
+    { text: "Orange Sky over Cornerstone Hall(?)", image: Pic11 }, //OrangeNearSeahawkLanding.jpg
+    { text: "Pink Sky in late March near Terrapin Hall", image: Pic12 }, //MarchSunset.jpg
+    { text: "Off-campus trail in early April", image: Pic13 }, //UNCWTrail.jpg
+    { text: "Off-campus trail in late August near the rope climbing course", image: Pic14 }, //TrailNearCourse.jpg
+    { text: "Beautiful Orange Sky", image: Pic15 }, //OrangeSkyOne.jpg
+    { text: "Theater Balcony in the Cultural Arts Building", image: Pic16 }, //Theater.jpg
+    { text: "Sunny Terrapin Study Room", image: Pic17 } //StudyRoom.jpg
 ]
 
 
 export const NorthCarolinaPics = [
-    { text: "I think this was taken in Mooresville. Photo creds to my dad", image: Pic16 }, //MooresvilleSunset.jpg
-    { text: "Rainbow in Weddington in July", image: Pic17 }, //WeddingtonRainbow.jpg
-    { text: "The BoardWalk in Charlotte. Late August", image: Pic18 }, //CharlotteBoardwalkView.jpg
-    { text: "Sunrise in Durham. (This looked way more pink and orange irl. Taken in early March)", image: Pic19 }, //SunriseInDurham.jpg
-    { text: "Pink Skys in March (Durham)", image: Pic20 }, //PinkSkyMarchDurham.jpg
-    { text: "Sunset through the buildings in Durham", image: Pic21 }, //RedSunsetMarch.jpg
-    { text: "Durham Sidewalk and Sunset", image: Pic22 }, //DurhamSidewalkSunset.jpg
-    { text: "Firey Clouds in Durham", image: Pic23 }, //FireyClouds.jpg
-    { text: "Rolling Clouds in Late February", image: Pic24 }, //RollingClouds.jpg
-    { text: "This is a picture I took when I was interning at USAA in Plano, Texas.", image: Pic25 } //Texas.jpg
+    { text: "I think this was taken in Mooresville. Photo creds to my dad", image: Pic18 }, //MooresvilleSunset.jpg
+    { text: "Rainbow in Weddington in July", image: Pic19 }, //WeddingtonRainbow.jpg
+    { text: "The BoardWalk in Charlotte. Late August", image: Pic20 }, //CharlotteBoardwalkView.jpg
+    { text: "Sunrise in Durham. (This looked way more pink and orange irl. Taken in early March)", image: Pic21 }, //SunriseInDurham.jpg
+    { text: "Pink Skys in March (Durham)", image: Pic22 }, //PinkSkyMarchDurham.jpg
+    { text: "Sunset through the buildings in Durham", image: Pic23 }, //RedSunsetMarch.jpg
+    { text: "Durham Sidewalk and Sunset", image: Pic24 }, //DurhamSidewalkSunset.jpg
+    { text: "Firey Clouds in Durham", image: Pic25 }, //FireyClouds.jpg
+    { text: "Rolling Clouds in Late February", image: Pic26 }, //RollingClouds.jpg
+    { text: "This is a picture I took when I was interning at USAA in Plano, Texas.", image: Pic27 } //Texas.jpg
     
 ]
 
