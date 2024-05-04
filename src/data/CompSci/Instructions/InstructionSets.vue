fc78c38920cd6e49fd3f1f868465abf54d27324f
Michael Totaro <mtotaro@ncsu.edu>
2024-05-04 18:07:24 -0400

    Seperated Instructions Into Different Files

---
+++ src/data/CompSci/Instructions/InstructionSets.vue
@@ -0,0 +1,24 @@
+<script>
+import SetupNode from './SetupNode.json';
+import SetupVue from './SetupVue.json';
+import SetupReact from './SetupReact.json';
+import GithubPages from './GithubPages.json';
+import ViteGHPages from './ViteGHPages.json';
+import IntellijJavaFX from './IntellijJavaFX.json';
+import EclipseJavaFX from './EclipseJavaFX.json';
+import SQLite3 from './SQLite3.json';
+import FlaskSQLite from './FlaskSQLite.json';
+
+export const AllSets = [
+    SetupNode,
+    SetupVue,
+    SetupReact,
+    GithubPages,
+    ViteGHPages,
+    IntellijJavaFX,
+    EclipseJavaFX,
+    SQLite3,
+    FlaskSQLite
+]
+
+</script>
