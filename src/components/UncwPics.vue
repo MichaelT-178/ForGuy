ef28f347612692fa7d3a37f54887488b888ec14b
Michael Totaro <mstotaro@gmail.com>
2024-05-22 22:38:10 -0400

    Added pics and memes

--- src/components/UncwPics.vue
+++ src/components/UncwPics.vue
@@ -16,6 +16,8 @@ import Pic14 from "../assets/UncwPics/TrailNearCourse.jpg";
 import Pic15 from "../assets/UncwPics/OrangeSkyOne.jpg";
 import Pic16 from "../assets/UncwPics/Theater.jpg";
 import Pic17 from "../assets/UncwPics/StudyRoom.jpg";
+
+//North Carolina pics
 import Pic18 from "../assets/UncwPics/MooresvilleSunset.jpg";
 import Pic19 from "../assets/UncwPics/WeddingtonRainbow.jpg";
 import Pic20 from "../assets/UncwPics/CharlotteBoardwalkView.jpg";
@@ -25,7 +27,22 @@ import Pic23 from "../assets/UncwPics/RedSunsetMarch.jpg";
 import Pic24 from "../assets/UncwPics/DurhamSidewalkSunset.jpg";
 import Pic25 from "../assets/UncwPics/FireyClouds.jpg";
 import Pic26 from "../assets/UncwPics/RollingClouds.jpg";
-import Pic27 from "../assets/UncwPics/Texas.jpg";
+import Pic27 from "../assets/UncwPics/IBMOfficeView.jpg";
+import Pic28 from "../assets/UncwPics/TrainTracks.jpg";
+import Pic29 from "../assets/UncwPics/BlueLight.jpg";
+import Pic30 from "../assets/UncwPics/IBMMarch.jpg";
+import Pic31 from "../assets/UncwPics/Dove.jpg";
+import Pic32 from "../assets/UncwPics/DurhamPond.jpg";
+
+
+//Texas pics
+import Pic33 from "../assets/UncwPics/OfficeView.jpg";
+import Pic34 from "../assets/UncwPics/Texas.jpg";
+import Pic35 from "../assets/UncwPics/Lanterns.jpg";
+import Pic36 from "../assets/UncwPics/PlanoBuildings.jpg";
+import Pic37 from "../assets/UncwPics/PlanoPond.jpg";
+import Pic38 from "../assets/UncwPics/USAASign.jpg";
+
 
 
 export const uncwPics = [
@@ -59,8 +76,21 @@ export const NorthCarolinaPics = [
     { text: "Durham Sidewalk and Sunset", image: Pic24 }, //DurhamSidewalkSunset.jpg
     { text: "Firey Clouds in Durham", image: Pic25 }, //FireyClouds.jpg
     { text: "Rolling Clouds in Late February", image: Pic26 }, //RollingClouds.jpg
-    { text: "This is a picture I took when I was interning at USAA in Plano, Texas.", image: Pic27 } //Texas.jpg
-    
+    { text: "View from my IBM Office", image: Pic27 }, //IBMOfficeView.jpg
+    { text: "NCSU Train Tracks", image: Pic28 }, //TrainTracks.jpg
+    { text: "Blue Light in Darkness", image: Pic29 }, //BlueLight.jpg
+    { text: "Spring in Durham", image: Pic30 }, //IBMMarch.jpg
+    { text: "Visitor Outside My Window", image: Pic31 }, //Dove.jpg
+    { text: "Durham Pond in Spring", image: Pic32 } //DurhamPond.jpg
+]
+
+export const TexasPics = [
+    { text: "View from my USAA office window", image: Pic33 }, //OfficeView.jpg
+    { text: "Apartment picture Taken from USAA balcony", image: Pic34 }, //Texas.jpg
+    { text: "Lantern Store in Plano, Texas", image: Pic35 }, //Lanterns.jpg
+    { text: "Cloudy day in Texas", image: Pic36 }, //PlanoBuildings.jpg
+    { text: "Picture of Pond in Texas", image: Pic37 }, //PlanoPond.jpg
+    { text: "USAA Sign in front of the building where I worked", image: Pic38 } //USAASign.jpg
 ]
 
 </script>
