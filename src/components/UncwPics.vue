0559e54b8b807d847af0f4bcef28b0ce555a9cc7
Michael Totaro <mstotaro@gmail.com>
2024-05-25 13:49:25 -0400

    Added pic and meme

--- src/components/UncwPics.vue
+++ src/components/UncwPics.vue
@@ -16,32 +16,33 @@ import Pic14 from "../assets/UncwPics/TrailNearCourse.jpg";
 import Pic15 from "../assets/UncwPics/OrangeSkyOne.jpg";
 import Pic16 from "../assets/UncwPics/Theater.jpg";
 import Pic17 from "../assets/UncwPics/StudyRoom.jpg";
+import Pic18 from "../assets/UncwPics/AugustSunset.jpg";
 
 //North Carolina pics
-import Pic18 from "../assets/UncwPics/MooresvilleSunset.jpg";
-import Pic19 from "../assets/UncwPics/WeddingtonRainbow.jpg";
-import Pic20 from "../assets/UncwPics/CharlotteBoardwalkView.jpg";
-import Pic21 from "../assets/UncwPics/SunriseInDurham.jpg";
-import Pic22 from "../assets/UncwPics/PinkSkyMarchDurham.jpg";
-import Pic23 from "../assets/UncwPics/RedSunsetMarch.jpg";
-import Pic24 from "../assets/UncwPics/DurhamSidewalkSunset.jpg";
-import Pic25 from "../assets/UncwPics/FireyClouds.jpg";
-import Pic26 from "../assets/UncwPics/RollingClouds.jpg";
-import Pic27 from "../assets/UncwPics/IBMOfficeView.jpg";
-import Pic28 from "../assets/UncwPics/TrainTracks.jpg";
-import Pic29 from "../assets/UncwPics/BlueLight.jpg";
-import Pic30 from "../assets/UncwPics/IBMMarch.jpg";
-import Pic31 from "../assets/UncwPics/Dove.jpg";
-import Pic32 from "../assets/UncwPics/DurhamPond.jpg";
+import Pic19 from "../assets/UncwPics/MooresvilleSunset.jpg";
+import Pic20 from "../assets/UncwPics/WeddingtonRainbow.jpg";
+import Pic21 from "../assets/UncwPics/CharlotteBoardwalkView.jpg";
+import Pic22 from "../assets/UncwPics/SunriseInDurham.jpg";
+import Pic23 from "../assets/UncwPics/PinkSkyMarchDurham.jpg";
+import Pic24 from "../assets/UncwPics/RedSunsetMarch.jpg";
+import Pic25 from "../assets/UncwPics/DurhamSidewalkSunset.jpg";
+import Pic26 from "../assets/UncwPics/FireyClouds.jpg";
+import Pic27 from "../assets/UncwPics/RollingClouds.jpg";
+import Pic28 from "../assets/UncwPics/IBMOfficeView.jpg";
+import Pic29 from "../assets/UncwPics/TrainTracks.jpg";
+import Pic30 from "../assets/UncwPics/BlueLight.jpg";
+import Pic31 from "../assets/UncwPics/IBMMarch.jpg";
+import Pic32 from "../assets/UncwPics/Dove.jpg";
+import Pic33 from "../assets/UncwPics/DurhamPond.jpg";
 
 
 //Texas pics
-import Pic33 from "../assets/UncwPics/OfficeView.jpg";
-import Pic34 from "../assets/UncwPics/Texas.jpg";
-import Pic35 from "../assets/UncwPics/Lanterns.jpg";
-import Pic36 from "../assets/UncwPics/PlanoBuildings.jpg";
-import Pic37 from "../assets/UncwPics/PlanoPond.jpg";
-import Pic38 from "../assets/UncwPics/USAASign.jpg";
+import Pic34 from "../assets/UncwPics/OfficeView.jpg";
+import Pic35 from "../assets/UncwPics/Texas.jpg";
+import Pic36 from "../assets/UncwPics/Lanterns.jpg";
+import Pic37 from "../assets/UncwPics/PlanoBuildings.jpg";
+import Pic38 from "../assets/UncwPics/PlanoPond.jpg";
+import Pic39 from "../assets/UncwPics/USAASign.jpg";
 
 
 
@@ -62,36 +63,37 @@ export const uncwPics = [
     { text: "Off-campus trail in late August near the rope climbing course", image: Pic14 }, //TrailNearCourse.jpg
     { text: "Beautiful Orange Sky", image: Pic15 }, //OrangeSkyOne.jpg
     { text: "Theater Balcony in the Cultural Arts Building", image: Pic16 }, //Theater.jpg
-    { text: "Sunny Terrapin Study Room", image: Pic17 } //StudyRoom.jpg
+    { text: "Sunny Terrapin Study Room", image: Pic17 }, //StudyRoom.jpg
+    { text: "Sunset in late August 2023", image: Pic18 } //AugustSunset.jpg
 ]
 
 
 export const NorthCarolinaPics = [
-    { text: "I think this was taken in Mooresville. Photo creds to my dad", image: Pic18 }, //MooresvilleSunset.jpg
-    { text: "Rainbow in Weddington in July", image: Pic19 }, //WeddingtonRainbow.jpg
-    { text: "The BoardWalk in Charlotte. Late August", image: Pic20 }, //CharlotteBoardwalkView.jpg
-    { text: "Sunrise in Durham. (This looked way more pink and orange irl. Taken in early March)", image: Pic21 }, //SunriseInDurham.jpg
-    { text: "Pink Skys in March (Durham)", image: Pic22 }, //PinkSkyMarchDurham.jpg
-    { text: "Sunset through the buildings in Durham", image: Pic23 }, //RedSunsetMarch.jpg
-    { text: "Durham Sidewalk and Sunset", image: Pic24 }, //DurhamSidewalkSunset.jpg
-    { text: "Firey Clouds in Durham", image: Pic25 }, //FireyClouds.jpg
-    { text: "Rolling Clouds in Late February", image: Pic26 }, //RollingClouds.jpg
-    { text: "View from my IBM Office", image: Pic27 }, //IBMOfficeView.jpg
-    { text: "NCSU Train Tracks in mid-April", image: Pic28 }, //TrainTracks.jpg
-    { text: "Blue Light in Darkness", image: Pic29 }, //BlueLight.jpg
-    { text: "Spring in Durham", image: Pic30 }, //IBMMarch.jpg
-    { text: "Visitor Outside My Window", image: Pic31 }, //Dove.jpg
-    { text: "Durham Pond in Spring", image: Pic32 } //DurhamPond.jpg
+    { text: "I think this was taken in Mooresville. Photo creds to my dad", image: Pic19 }, //MooresvilleSunset.jpg
+    { text: "Rainbow in Weddington in July", image: Pic20 }, //WeddingtonRainbow.jpg
+    { text: "The BoardWalk in Charlotte. Late August", image: Pic21 }, //CharlotteBoardwalkView.jpg
+    { text: "Sunrise in Durham. (This looked way more pink and orange irl. Taken in early March)", image: Pic22 }, //SunriseInDurham.jpg
+    { text: "Pink Skys in March (Durham)", image: Pic23 }, //PinkSkyMarchDurham.jpg
+    { text: "Sunset through the buildings in Durham", image: Pic24 }, //RedSunsetMarch.jpg
+    { text: "Durham Sidewalk and Sunset", image: Pic25 }, //DurhamSidewalkSunset.jpg
+    { text: "Firey Clouds in Durham", image: Pic26 }, //FireyClouds.jpg
+    { text: "Rolling Clouds in Late February", image: Pic27 }, //RollingClouds.jpg
+    { text: "View from my IBM Office", image: Pic28 }, //IBMOfficeView.jpg
+    { text: "NCSU Train Tracks in mid-April", image: Pic29 }, //TrainTracks.jpg
+    { text: "Blue Light in Darkness", image: Pic30 }, //BlueLight.jpg
+    { text: "Spring in Durham", image: Pic31 }, //IBMMarch.jpg
+    { text: "Visitor Outside My Window", image: Pic32 }, //Dove.jpg
+    { text: "Durham Pond in Spring", image: Pic33 } //DurhamPond.jpg
 ]
 
 
 export const TexasPics = [
-    { text: "View from my USAA office window", image: Pic33 }, //OfficeView.jpg
-    { text: "Apartment picture Taken from USAA balcony", image: Pic34 }, //Texas.jpg
-    { text: "Lantern Store in Plano, Texas", image: Pic35 }, //Lanterns.jpg
-    { text: "Cloudy day in Texas", image: Pic36 }, //PlanoBuildings.jpg
-    { text: "Picture of Pond in Texas", image: Pic37 }, //PlanoPond.jpg
-    { text: "USAA Sign in front of the building where I worked", image: Pic38 } //USAASign.jpg
+    { text: "View from my USAA office window", image: Pic34 }, //OfficeView.jpg
+    { text: "Apartment picture Taken from USAA balcony", image: Pic35 }, //Texas.jpg
+    { text: "Lantern Store in Plano, Texas", image: Pic36 }, //Lanterns.jpg
+    { text: "Cloudy day in Texas", image: Pic37 }, //PlanoBuildings.jpg
+    { text: "Picture of Pond in Texas", image: Pic38 }, //PlanoPond.jpg
+    { text: "USAA Sign in front of the building where I worked", image: Pic39 } //USAASign.jpg
 ]
 
 </script>
