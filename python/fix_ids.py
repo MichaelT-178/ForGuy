e23cb92880afb51e0c84b797603125f370708211
Michael Totaro <mstotaro@gmail.com>
2024-06-09 18:32:36 -0400

    Added data

---
+++ python/fix_ids.py
@@ -0,0 +1,15 @@
+import json 
+
+
+file_path = "../src/data/SearchPages.json"
+
+with open(file_path, 'r') as file:
+    data = json.load(file)
+
+    for i, item in enumerate(data):
+        item["id"] = i + 1
+
+    
+with open(file_path, 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
+    print("Updated Successfully!")
