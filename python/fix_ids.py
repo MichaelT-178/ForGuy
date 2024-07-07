2513e7da58427a9dce2464265fe86d681f25c4e0
Michael Totaro <mstotaro@gmail.com>
2024-07-07 14:46:33 -0400

    EmailJS and Videos

--- python/fix_ids.py
+++ python/fix_ids.py
@@ -3,24 +3,30 @@ import json
 
 # file_path = "../src/data/SearchPages.json"
 # file_path = "../src/data/CompSci/Instructions/SwiftData.json"
-file_path = "../src/data/CompSci/Instructions/RedditBotAWS.json"
+# file_path = "../src/data/CompSci/Instructions/RedditBotAWS.json"
 # file_path = "../src/data/CompSci/Instructions/SpringApp.json"
 # file_path = "../src/data/CompSci/Instructions/NodeServer.json"
+
+file_path = "../src/data/CompSci/Instructions/EmailJS.json"
+
 
 with open(file_path, 'r') as file:
     data = json.load(file)
     
+    # For normal files
     # for i, item in enumerate(data):
     #     item["id"] = i + 1
 
 
+    # For One set Instructions 
     # data_set = data["Instructions"]
     
     # for i, item in enumerate(data_set):
     #     item["id"] = i + 1
     
     
-    data_set = data["MultiSet"][5]["Instructions"]
+    # For MultiSet Instructions
+    data_set = data["MultiSet"][0]["Instructions"]
 
     for i, item in enumerate(data_set):
         item["id"] = i + 1
