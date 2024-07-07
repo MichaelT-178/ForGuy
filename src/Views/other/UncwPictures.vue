f0dcdfb8fbfa70197fce03adae72824b0fcd6a25
Michael Totaro <mstotaro@gmail.com>
2024-07-07 14:46:33 -0400

    EmailJS and Videos

--- src/Views/other/UncwPictures.vue
+++ src/Views/other/UncwPictures.vue
@@ -5,7 +5,15 @@
         
         <div class="uncw-pics-container" v-for="(pic, index) in uncwPics" :key="index">
             <div class="uncw-pic-text">{{ index + 1 }}. {{ pic.text }}</div>
-            <img :src="pic.image" alt="Uncw Picture">
+            <div v-if="pic.video">
+                <div class="vertical-video-container">
+                    <video controls>
+                        <source :src="pic.video" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+            </div>
+            <img v-else :src="pic.image" alt="Uncw Picture">
         </div>
         
         <h1>{{ text.header2 }}</h1>
@@ -13,15 +21,30 @@
         
         <div class="uncw-pics-container" v-for="(pic, index) in NorthCarolinaPics" :key="index">
             <div class="uncw-pic-text">{{ index + 1 }}. {{ pic.text }}</div>
-            <img :src="pic.image" alt="Uncw Picture">
-        </div>
+                <div v-if="pic.video">
+                    <video class="horizontal-video-container" controls>
+                        <source :src="pic.video" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+
+                <img v-else :src="pic.image" alt="Uncw Picture">
+            </div>
 
         <h1>{{ text.header3 }}</h1>
         <p>{{ text.desc3 }}</p>
         
         <div class="uncw-pics-container" v-for="(pic, index) in TexasPics" :key="index">
             <div class="uncw-pic-text">{{ index + 1 }}. {{ pic.text }}</div>
-            <img :src="pic.image" alt="Uncw Picture">
+                <div v-if="pic.video">
+                    <video class="horizontal-video-container" controls>
+                        <source :src="pic.video" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <p style="padding-bottom: 50px;"></p>
+                </div>
+
+                <img v-else :src="pic.image" alt="Uncw Picture">
         </div>
     </div>
 </template>
@@ -50,6 +73,30 @@ const text = jsonData.value["Text"][0];
     align-items: flex-start;
     width: 400px;
     margin-bottom: 35px;
+}
+
+.horizontal-video-container {
+    width: 600px;
+    border: 2px solid black;
+    margin-top: 8px;
+}
+
+.vertical-video-container {
+    width: 400px;
+    height: 575px;
+    margin-top: 8px;
+    overflow: hidden;
+    position: relative;
+    border: 2px solid black;
+}
+
+.vertical-video-container video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
 
 .uncw-pic-text {
