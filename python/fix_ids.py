677de211c9d90bfe70cca4610325c41fe21da804
Michael Totaro <mstotaro@gmail.com>
2024-07-21 21:59:25 -0400

    FlashDrive and FormatCode

--- python/fix_ids.py
+++ python/fix_ids.py
@@ -1,29 +1,29 @@
 import json 
 from termcolor import colored as c
 
-# file_path = "../src/data/SearchPages.json"
+file_path = "../src/data/SearchPages.json"
 # file_path = "../src/data/CompSci/Instructions/SwiftData.json"
 # file_path = "../src/data/CompSci/Instructions/RedditBotAWS.json"
 # file_path = "../src/data/CompSci/Instructions/SpringApp.json"
 # file_path = "../src/data/CompSci/Instructions/NodeServer.json"
 
 # file_path = "../src/data/CompSci/Instructions/EmailJS.json"
-file_path = "../src/data/CompSci/Instructions/PyiCloud.json"
+# file_path = "../src/data/CompSci/Instructions/PyiCloud.json"
 
 
 with open(file_path, 'r') as file:
     data = json.load(file)
     
     # For normal files
-    # for i, item in enumerate(data):
-    #     item["id"] = i + 1
-
-
-    # For One set Instructions 
-    data_set = data["Instructions"]
-    
-    for i, item in enumerate(data_set):
+    for i, item in enumerate(data):
         item["id"] = i + 1
+
+
+    # # For One set Instructions 
+    # data_set = data["Instructions"]
+    
+    # for i, item in enumerate(data_set):
+    #     item["id"] = i + 1
     
     
     # For MultiSet Instructions
