2ebbb177c95b41c8223d1678eb15a907cb883602
Michael Totaro <mstotaro@gmail.com>
2024-07-25 14:16:21 -0400

    Added data

--- src/Views/other/memes.vue
+++ src/Views/other/memes.vue
@@ -57,7 +57,7 @@ const text = jsonData.value["Text"][0];
     position: absolute;
     width: 100%;
     height: 100%;
-    top: 0;
+    top: 200px;
     left: 0;
     z-index: -1;
     pointer-events: none;
